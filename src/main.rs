7f4385e0804f07f17e45e98d6adb847de87e0262
Hector Carrillo <technik.und.spiel@gmail.com>
2022-10-16 22:52:00 -0500

    [feat] read user strings and check if they are valid given alphabet

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,56 @@ use std::io;
 use std::collections::HashSet; 
 
 fn main() {
-  let language_input_type: String = read_language_input_type();
-  let mut user_language: HashSet<String> = HashSet::new();
-  // if the user input type is 1, read the language from range, else from a list
-  if language_input_type == "1" {
-    user_language = read_language_from_range();
-  } else if language_input_type == "2" {
-    user_language = read_language_from_list();
-  }
-  // print the language
-  println!("Your language is: {:?}", user_language);
+  let user_alphabet: HashSet<String> = read_user_defined_alphabet();
+  // print the alphabet
+  println!("El alfabeto es: {:?}", user_alphabet);
+  // read two strings and check if they are valid
+  let string1 = read_valid_string(&user_alphabet);
+  let string2 = read_valid_string(&user_alphabet);
+  // print the strings
+  println!("La primera cadena es: {}", string1);
+  println!("La segunda cadena es: {}", string2);
 }
 
-fn read_language_input_type() -> String {
+fn read_valid_string(alphabet: &HashSet<String>) -> String {
+  println!("Ingrese una cadena valida:");
+  let mut valid_string: String = String::new();
+  while valid_string == "" {
+    let user_input_string = read_string();
+    if is_valid_string(&user_input_string, &alphabet){
+      valid_string = user_input_string.clone();
+    } else {
+      println!("La cadena ingresada no es valida, intente nuevamente:");
+    }
+  }
+  return valid_string;
+}
+
+fn read_user_defined_alphabet() -> HashSet<String> {
+  let alphabet_input_type: String = read_alphabet_input_type();
+  let mut user_alphabet: HashSet<String> = HashSet::new();
+  // if the user input type is 1, read the alphabet from range, else from a list
+  while user_alphabet.len() == 0 {
+    if alphabet_input_type == "1" {
+      user_alphabet = read_alphabet_from_range();
+    } else if alphabet_input_type == "2" {
+      user_alphabet = read_alphabet_from_list();
+    }
+    if user_alphabet.len() == 0 {
+      println!("El alfabeto ingresado no es valido, intente nuevamente:");
+    }
+  }
+  return user_alphabet;
+}
+
+fn read_string() -> String {
+  let mut input = String::new();
+  println!("Please enter a string: ");
+  io::stdin().read_line(&mut input).expect("Failed to read line");
+  return input.trim().to_string();
+}
+
+fn read_alphabet_input_type() -> String {
   let stdin = io::stdin();
 
   println!("Seleccione la forma de ingresar el alfabeto:");
@@ -27,33 +64,47 @@ fn read_language_input_type() -> String {
   return input_form.trim().to_string();
 }
 
-fn read_language_from_list () -> HashSet<String> {
+fn read_alphabet_from_list () -> HashSet<String> {
   let stdin = io::stdin();
-  let mut user_language: HashSet<String> = HashSet::new();
+  let mut user_alphabet: HashSet<String> = HashSet::new();
   
   println!("Ingrese el alfabeto de la gramática, separado por espacios:");
-  let mut input_language = String::new();
-  stdin.read_line(&mut input_language);
+  let mut input_alphabet = String::new();
+  stdin.read_line(&mut input_alphabet);
   
-  for symbol in input_language.split_whitespace() {
-    user_language.insert(symbol.to_string());
+  for symbol in input_alphabet.split_whitespace() {
+    user_alphabet.insert(symbol.to_string());
   }
   
-  return user_language;
+  return user_alphabet;
 }
 
-fn read_language_from_range () -> HashSet<String> {
+fn read_alphabet_from_range () -> HashSet<String> {
   println!("Ingrese el rango de caracteres del alfabeto de la siguiente forma: <primer_caracter>-<ultimo_caracter>");
-  let mut user_input_range = String::new();
-  let stdin = io::stdin();
-  stdin.read_line(&mut user_input_range);
+  let user_input_range = read_string();
 
-  let first_language_char_as_int: i32 = user_input_range.chars().nth(0).unwrap() as i32;
-  let last_language_char_as_int: i32 = user_input_range.chars().nth(2).unwrap() as i32;
+  let first_alphabet_char_as_int: i32 = user_input_range.chars().nth(0).unwrap() as i32;
+  let last_alphabet_char_as_int: i32 = user_input_range.chars().nth(2).unwrap() as i32;
   
-  let mut user_language: HashSet<String> = HashSet::new();
-  for i in first_language_char_as_int..last_language_char_as_int {
-    user_language.insert((i as u8 as char).to_string());
+  let mut user_alphabet: HashSet<String> = HashSet::new();
+  for i in first_alphabet_char_as_int..=last_alphabet_char_as_int {
+    user_alphabet.insert((i as u8 as char).to_string());
   }
-  return user_language;
+  return user_alphabet;
+}
+
+fn is_valid_string (string: &String, alphabet: &HashSet<String>) -> bool {
+  if string.len() == 0 {
+    return true;
+  }
+  let mut current_symbol: String = String::new();
+  let mut final_valid_string: String = String::new();
+  for character in string.chars() {
+    current_symbol.push(character);
+    if alphabet.contains(&current_symbol) {
+      final_valid_string.push_str(&current_symbol);
+      current_symbol = String::new();
+    }
+  }
+  return string.len() == final_valid_string.len();
 }
